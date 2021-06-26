2cb6f5b464001bdbd20817d1b4ec0a4a5d175eb3
leocassiosilva <leocassiosilva1234@gmail.com>
2021-06-25 22:46:19 -0300

    contador

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  
+  const [count, setCount] = useState(0);
+  
+  function aumentar(){
+    setCount(count+1)
+  }
+
+  function diminuir(){
+    setCount(count-1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.countContainer}>
+        <Text style={styles.textTitle}>Contador {count} </Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={aumentar}>
+        <Text style={styles.text}>+</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={diminuir}>
+        <Text style={styles.text}>-</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +36,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10, 
+    height:30,
+    width:30,
+    margin:20
+  },
+
+  countContainer: {
+    alignItems: "center",
+    padding: 10
   },
 });
